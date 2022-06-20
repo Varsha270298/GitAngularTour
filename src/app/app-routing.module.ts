1e926da902804c5ef8031f1cd591d180fb486591
shaikarif155 <83698560+shaikarif155@users.noreply.github.com>
2022-06-20 22:19:23 +0530

    By arif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TourinfoEditComponent } from './tourinfo-edit/tourinfo-edit.component';
+import { TourinfoComponent } from './tourinfo/tourinfo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'tourinfo',component: TourinfoComponent},
+  {path:'tourinfo-edit',component:TourinfoEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TourinfoComponent } from './tourinfo/tourinfo.component';
+import { HttpClientModule } from "@angular/common/http";
+import { TourinfoEditComponent } from './tourinfo-edit/tourinfo-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TourinfoComponent,
+    TourinfoEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
