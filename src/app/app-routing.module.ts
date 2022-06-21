ebcc9e820d1a2738701877de8c01e2a06d006709
capsha22 <shashank.b.sharma@capgemini.com>
2022-06-21 10:23:24 +0530

    by sha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTourComponent } from './add-tour/add-tour.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'addtour', component: AddTourComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminAddService } from './admin-add.service';
+import {HttpClientModule} from '@angular/common/http';
+import { AddTourComponent } from './add-tour/add-tour.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTourComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
