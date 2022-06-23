6f6bcd82fb43321b97336fd510e4b3c914e4d855
shaikarif155 <83698560+shaikarif155@users.noreply.github.com>
2022-06-23 21:34:57 +0530

    by arif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,44 +14,28 @@ import { TourinfoListComponent } from './tourinfo-list/tourinfo-list.component';
 import { TourinfoComponent } from './tourinfo/tourinfo.component';
 
 
+
 const routes: Routes =[
 { path:'addtour', component: AddTourComponent },
 {path:'tourinfo',component:TourinfoComponent},
 {path:'tourinfo/:id',component:TourinfoEditComponent },
 {path:'customer',component:CustomerComponent},
 {path:'login',component:LoginComponent},
-
-
-
-
-{ path: 'edittour', component:EditTourInformationSystemComponent },
-
-
+{path: 'edittour', component:EditTourInformationSystemComponent },
 { path: 'edittour', component:EditTourInformationSystemComponent },
 { path: 'listtour', component: TourInformationSystemListComponent},
 { path: 'CustTourList', component:CustomerTourListComponent },
 { path: 'restourlist', component:TourinfoListComponent },
-<<<<<<< HEAD
-
-=======
 { path: 'edittour', component:EditTourInformationSystemComponent },
->>>>>>> fd96caaea06dcef62b13f226b5d40f295b1457fe
->>>>>>> 5771fc3d5119f3a3706edfbdd34db01eb823cbf6
 { path: 'add-user', component: AddUserComponent },
 { path: 'list-user', component: ListUserComponent },
 { path: 'edit-user', component: EditUserComponent },
-//{path:'register', component:RegisterComponent},
-<<<<<<< HEAD
-
 { path: 'listtour', component: TourInformationSystemListComponent},
 { path: 'edittour', component:EditTourInformationSystemComponent },
 {path:'viewtour',component:TourInformationSystemListComponent},
 
-{ path: 'listtour', component: TourInformationSystemListComponent},
 
-=======
-{path:'viewtour',component:TourInformationSystemListComponent},
->>>>>>> fd96caaea06dcef62b13f226b5d40f295b1457fe
+
 
 ];
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { BrowserModule } from '@angular/platform-browser';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddStaffComponent } from './add-staff/add-staff.component';
-=======
-
 import { BrowserModule } from '@angular/platform-browser';
-
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-<<<<<<< HEAD
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-
-=======
-
-
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
->>>>>>> fd96caaea06dcef62b13f226b5d40f295b1457fe
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminAddService } from './admin-add.service';
 import { AddTourComponent } from './add-tour/add-tour.component';
@@ -35,67 +12,33 @@ import { TourinfoEditComponent } from './tourinfo-edit/tourinfo-edit.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { ListUserComponent } from './list-user/list-user.component';
-import { CustomerComponent } from './customer/customer.component';
 import { TourInformationSystemListComponent } from './tour-information-system-list/tour-information-system-list.component';
-
 import { CustomerTourListComponent } from './customer-tour-list/customer-tour-list.component';
 import { TourinfoListComponent } from './tourinfo-list/tourinfo-list.component';
-
 import { TokenInterceptor } from './core/interceptor';
 import { ApiService } from './core/api.service';
 import { EditTourInformationSystemComponent } from './edit-tour-information-system/edit-tour-information-system.component';
-<<<<<<< HEAD
-import { RouterModule } from '@angular/router';
+
 import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
 import { StaffDashboardComponent } from './staff-dashboard/staff-dashboard.component';
-
-=======
-<<<<<<< HEAD
 import { CustomerComponent } from './customer/customer.component';
-=======
->>>>>>> fd96caaea06dcef62b13f226b5d40f295b1457fe
-//import { RegisterComponent } from './register/register.component';
->>>>>>> 5771fc3d5119f3a3706edfbdd34db01eb823cbf6
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 
-
-
-<<<<<<< HEAD
-=======
-
->>>>>>> fd96caaea06dcef62b13f226b5d40f295b1457fe
->>>>>>> 7eba1d887763ba60ac47b20a9478ea393b162346
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    AddStaffComponent
-   
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-
-    
-=======
-<<<<<<< HEAD
-
+    AddStaffComponent,
     LoginComponent,
-  
-
-=======
-    LoginComponent,
->>>>>>> fd96caaea06dcef62b13f226b5d40f295b1457fe
-    EditTourInformationSystemComponent,
+     EditTourInformationSystemComponent,
     CustomerComponent,
     AddUserComponent,
     EditUserComponent,
     ListUserComponent,
-    LoginComponent,
     TourinfoComponent,
     TourinfoEditComponent,
     AddTourComponent,
@@ -103,34 +46,17 @@ import { CustomerComponent } from './customer/customer.component';
     CustomerTourListComponent,
     TourinfoListComponent,
     CustomerDashboardComponent,
-    StaffDashboardComponent
-   
-   
-<<<<<<< HEAD
-=======
-   
-
->>>>>>> fd96caaea06dcef62b13f226b5d40f295b1457fe
-  ],
+    StaffDashboardComponent,
+    ],
   imports: [
     BrowserModule,
-    FormsModule,
-<<<<<<< HEAD
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule,
-=======
-    ReactiveFormsModule,
-<<<<<<< HEAD
-    HttpClientModule,
-    AppRoutingModule
-   
-=======
+    FormsModule,
+    AppRoutingModule,
     HttpClientModule
->>>>>>> fd96caaea06dcef62b13f226b5d40f295b1457fe
->>>>>>> 7eba1d887763ba60ac47b20a9478ea393b162346
->>>>>>> 5771fc3d5119f3a3706edfbdd34db01eb823cbf6
-  ],
+],
+  
   providers: [ApiService, {provide: HTTP_INTERCEPTORS,
     useClass: TokenInterceptor,
     multi : true}],
