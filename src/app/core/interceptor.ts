fd69889eeae5ff368f22b1b594311daa76f63367
Kalyani6518 <106234859+Kalyani6518@users.noreply.github.com>
2022-06-24 18:25:15 +0530

    by kalyani

--- src/app/core/interceptor.ts
+++ src/app/core/interceptor.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs/internal/Observable";
 import {Injectable} from "@angular/core";
@@ -18,26 +17,3 @@ export class TokenInterceptor implements HttpInterceptor {
     return next.handle(request);
   }
 }
-=======
-
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs/internal/Observable";
-import {Injectable} from "@angular/core";
-
-@Injectable()
-export class TokenInterceptor implements HttpInterceptor {
-
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let token = window.localStorage.getItem('token');
-    if (token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: 'Bearer ' + token
-        }
-      });
-    }
-    return next.handle(request);
-  }
-}
-
->>>>>>> 33bfac33becbfb2b5aa454c8e64861c204564255
