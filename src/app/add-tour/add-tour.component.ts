64e8c016d804260cbfc6529a0113f30855c58b75
capsha22 <shashank.b.sharma@capgemini.com>
2022-06-26 17:33:17 +0530

    by sha

--- src/app/add-tour/add-tour.component.ts
+++ src/app/add-tour/add-tour.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import {FormBuilder, FormControl, FormGroup, MinLengthValidator, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import { AdminAddService } from '../admin-add.service';
+import { Tour } from '../tour';
 
 @Component({
   selector: 'app-add-tour',
@@ -9,16 +11,58 @@ import { AdminAddService } from '../admin-add.service';
   styleUrls: ['./add-tour.component.css']
 })
 export class AddTourComponent implements OnInit {
+  // addtour: Tour = new Tour();
+
+  // addForm = new FormGroup({
+  //   packageName: new FormControl('',[Validators.required,Validators.minLength(5)]),
+  //   description: new FormControl('',[Validators.required, Validators.minLength(5)]),
+  //   hotel: new FormControl('',[Validators.required ]),
+  //   amountPerPerson: new FormControl(''),
+  //   numberOfDays: new FormControl(''),
+  //   modeOfTransportation: new FormControl(''),
+  //    packageId: new FormControl('')
+
+  // });
+  
+ 
+   
+  
+  // constructor(private adminAddService: AdminAddService,
+  //   private router: Router) { }
+
+  // ngOnInit(): void {
+  // }
+  // savePackage(){
+  //   // this.apiService.addTour(this.addForm.value)
+  //   this.adminAddService.addTour(this.addForm.value).subscribe( data =>{
+  //     console.log(data);
+  //     this.goToTourList();
+  //   },
+  //   error => console.log(error));
+  // }
+
+  //   goToTourList(){
+  //     this.router.navigate(['/listtour']);
+  //   }
+
+    
+  //   onSubmit(){
+  //     // console.log(this.addtour);
+  //     console.log('Package value', this.addForm.value)
+  //     this.savePackage();
+  //   }
+
 
   message:any;
 
   constructor(private formBuilder: FormBuilder,private router: Router,private apiService: AdminAddService) { }
   addForm!: FormGroup;
-
+  
   ngOnInit(): void {
     this.addForm = this.formBuilder.group({
-      id: [],
+    
       packageId: ['', Validators.required],
+      
       packageName: ['', Validators.required],
       description: ['', Validators.required],
       numberOfDays: ['', Validators.required],
@@ -27,10 +71,14 @@ export class AddTourComponent implements OnInit {
       amountPerPerson: ['', Validators.required]
 
     });
+    // get packageName() { return this.addForm.get('packageName'); }
   }
 
+  // get f() { return this.addForm.controls; }
   onSubmit() {
+    this.router.navigate(['listtour']);
     this.apiService.addTour(this.addForm.value)
+    
      
       // .subscribe( data =>this.message=data);
       .subscribe( data => {
@@ -38,6 +86,9 @@ export class AddTourComponent implements OnInit {
         this.router.navigate(['listtour']);
       });
   }
+  Booking(){
+    alert('Tour Has Been Booked Successfully');
+    this.router.navigate(['listtour']);
+}
 
 }
- 
--- src/app/edit-tour-information-system/edit-tour-information-system.component.ts
+++ src/app/edit-tour-information-system/edit-tour-information-system.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Inject} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AdminAddService } from '../admin-add.service';
 import {first} from "rxjs/operators";
+import { Tour } from '../tour';
 
 @Component({
   selector: 'app-edit-tour-information-system',
@@ -10,19 +11,26 @@ import {first} from "rxjs/operators";
   styleUrls: ['./edit-tour-information-system.component.css']
 })
 export class EditTourInformationSystemComponent implements OnInit {
-  editForm!: FormGroup;
  
+  editForm!: FormGroup;
+  tour!: Tour;
   submitted = false;
   constructor(private formBuilder: FormBuilder,private router:Router,private apiService:AdminAddService) { }
   ngOnInit() { 
-    // let packageId = window.localStorage.getItem("packageId");
+      // this._route.paramMap.subscribe(parameterMap =>{
+      //   const id =+parameteterMap.getItem('editPackageId')
+      // })
+
+
+      let tourId = window.localStorage.getItem("editPackageId");
+      console.log(tourId)
     // if(!packageId) {
     //   alert("Invalid action.")
     //   this.router.navigate(['viewtour']);
     //   return;
     // }
   this.editForm = this.formBuilder.group({
-    packageId: [''],
+    packageId!: [''],
 packageName: ['', Validators.required],
 description: ['', Validators.required],
 numberOfDays: ['', Validators.required],
@@ -30,10 +38,12 @@ amountPerPerson: ['', Validators.required],
 modeOfTransportation: ['', Validators.required],
 hotel: ['', Validators.required]
 });
-//  this.apiService.getTourById(+packageId)
-// .subscribe( (data: { result: { [key: string]: any; }; }) => {
+ console.log( this.apiService.getTourById(+!tourId))
+//  .subscribe( data => {
 //   this.editForm.setValue(data.result);
-//  });
+//   });
+  
+
   }
 onSubmit() {
   this.apiService.updateTour(this.editForm.value)
